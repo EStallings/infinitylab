45541cf8f5c8a7f36c13e9c3255d3acc40929bb6
Sixstring982 <Sixstring982@gmail.com>
2014-11-08 16:15:33 -0700

    Docs for Screen.dart

--- web/inc/Screen.dart
+++ web/inc/Screen.dart
@@ -8,9 +8,20 @@ abstract class Screen {
   static const int WIDTH = 800;
   static const double FPS = 30.0;
 
+  /**
+   * Called when a MouseUp event happens at any location on the web page.
+   */
   /* abstract */ void onMouseUp(InteractionData e);
 
+  /**
+   * Called when this Screen should update itself, once per frame.
+   *
+   * @returns false if this Screen should be disposed of, else false.
+   */
   /* abstract */ bool update(ScreenStack stack);
 
+  /**
+   * Called when this Screen should render itself, once per frame.
+   */
   /* abstract */ void render(Stage stage);
 }
